25225c8fa1ca81c1b0f5440ea5e7810299208535
Diego T. Fialho <diego.tfialho@gmail.com>
2016-12-20 15:25:05 -0200

    Service created

--- client/app/foto/foto.module.ts
+++ client/app/foto/foto.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { FotoComponent } from './foto.component';
 import { FiltroPorTitulo } from './foto.pipes';
+import { FotoService } from './foto.service';
 
 @NgModule({
-	declarations: [ FotoComponent, FiltroPorTitulo ],
-	exports: [ FotoComponent, FiltroPorTitulo ]
+	declarations: [ FotoComponent, FiltroPorTitulo, FotoService ],
+	exports: [ FotoComponent, FiltroPorTitulo, FotoService ],
+	providers: [ FotoService ]
 })
 
 export class FotoModule {  }
